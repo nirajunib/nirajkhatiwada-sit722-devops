8d0b25129ca68e5a36496624e95604d8faf65062
nirajunib <s224311193@deakin.edu.au>
2024-09-21 14:35:09 +1000

    fix path

--- scripts/push-image.sh
+++ scripts/push-image.sh
@@ -5,7 +5,7 @@ set -u
 : "${CONTAINER_REGISTRY:=bookstorepart5.azurecr.io}"
 : "${VERSION:=latest}"
 : "${REGISTRY_UN:=bookstorepart5}"
-: "${REGISTRY_PW:=g9TE6sJmPH6i2j6yBwv5Q/fEO7g/YJS3ypPen3KKb3+ACRCWgRoe}"
+: "${REGISTRY_PW:=redacted}"
 
 set -e
 
